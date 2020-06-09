0a47b4b946fd36c84b4c9920b0d9cc473c11203d
Bene <bk191414@gmail.com>
2020-06-09 12:13:33 +0200

    init

--- onboarding-test/ViewController.swift
+++ onboarding-test/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        blurView.isHidden = true
     }
 
-
+    @IBAction func btnAdd(_ sender: UIButton) {
+        
+        blurView.isHidden = false
+        
+        
+        //blurView.effect = UIVisualEffectView(frame: blurView)
+    }
+    
+    @IBAction func btnButtonDoAll(_ sender: Any) {
+        
+        if blurView.isHidden {
+            
+                blurView.isHidden = false
+        } else {
+            
+            blurView.isHidden = true
+        }
+        
+        
+    }
+    
+    @IBAction func btnEnableDIsable(_ sender: UIButton) {
+        
+        
+        blurView.isUserInteractionEnabled = true
+        
+    }
 }
 
