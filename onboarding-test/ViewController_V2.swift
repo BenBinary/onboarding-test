75d0c3acb21564c24fe68eca357118b956f21e1d
Bene <bk191414@gmail.com>
2020-06-10 15:07:36 +0200

    It includes an animation

--- onboarding-test/ViewController_V2.swift
+++ onboarding-test/ViewController_V2.swift
@@ -24,6 +24,22 @@ class ViewController_V2: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        
+        let object = scnkitview.scene?.rootNode.childNode(withName: "ABS_5QM_OFFEN", recursively: true)
+        
+        
+        let spin = CABasicAnimation(keyPath: "rotation")
+        // Use from-to to explicitly make a full rotation around z
+        spin.fromValue = NSValue(scnVector4: SCNVector4(x: 0, y: 0, z: 1, w: 0))
+        spin.toValue = NSValue(scnVector4: SCNVector4(x: 0, y: 0, z: 1, w: Float(CGFloat(2 * M_PI))))
+        spin.duration = 3
+        spin.repeatCount = .infinity
+        object!.addAnimation(spin, forKey: "spin around")
+        
+        
+        //object?.addAnimation(an, forKey: <#T##String?#>)
     }
     
 
