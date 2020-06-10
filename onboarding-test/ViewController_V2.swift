9023264214bb8b0e79c0737cc12e9a3c0f7361f6
Bene <bk191414@gmail.com>
2020-06-10 14:16:01 +0200

    Still some Problems left

---
+++ onboarding-test/ViewController_V2.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController_V2.swift
+//  onboarding-test
+//
+//  Created by Benedikt Kurz on 10.06.20.
+//  Copyright © 2020 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_V2: UIViewController {
+    
+    
+    @IBOutlet weak var btnAbout: UIButton!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var btnScreenshot: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
